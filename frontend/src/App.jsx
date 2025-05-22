75ed700f19d869b0622234d3d160ca412aac9cb2
Harshada Shashank Yadav <137686756+HarshadaSYadav@users.noreply.github.com>
2025-05-22 23:50:44 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 		queryKey: ["authUser"],
 		queryFn: async () => {
 			try {
-				const res = await fetch("/api/auth/me");
+				const res = await fetch("https://x-clone-backend-tau.vercel.app/api/auth/me");
 				const data = await res.json();
 				if (data.error) return null;
 				if (!res.ok) {
